0501e3340c5591a4723effe139f5e9594a864395
Win <250845361@qq.com>
2017-10-20 15:44:34 +0800

    handsfree编译2017102004

--- handsfree/build/catkin_generated/setup_cached.sh
+++ handsfree/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,10 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/yobot/ROS/handsfree/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/yobot/ROS/handsfree/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/yobot/ROS/handsfree/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/yobot/ROS/handsfree/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/yobot/ROS/handsfree/build"
+export PYTHONPATH="/home/yobot/ROS/handsfree/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/yobot/ROS/handsfree/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/yobot/ROS/handsfree/src:$ROS_PACKAGE_PATH"
