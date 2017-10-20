54ee4fb57c6b33e9013d66d1275f13b09bfee7e7
Win <250845361@qq.com>
2017-10-20 16:02:43 +0800

    Delete catkin_ws 2017102005

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/yobot/ROS/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/yobot/catkin_ws/devel;/home/yobot/robot/devel;/opt/ros/indigo".split(';') if "/home/yobot/catkin_ws/devel;/home/yobot/robot/devel;/opt/ros/indigo" != "" else []
