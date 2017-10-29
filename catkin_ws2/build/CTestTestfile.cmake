231acfce7846e10ade9c68d6c65b3959d24a4d26
Win <250845361@qq.com>
2017-10-29 18:31:55 +0800

    add catkin_ws2 2017102901

---
+++ catkin_ws2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yobot/ROS/catkin_ws2/src
+# Build directory: /home/yobot/ROS/catkin_ws2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(chapter2_tutorials)
---
+++ catkin_ws2/build/chapter2_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yobot/ROS/catkin_ws2/src/chapter2_tutorials
+# Build directory: /home/yobot/ROS/catkin_ws2/build/chapter2_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
